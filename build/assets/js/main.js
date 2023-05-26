f2c4052df4fcd4240bdc3c89972e26ccef4c1908
RusudanNovikova <rusudan0@gmail.com>
2023-05-26 13:43:31 +0300

    26.05.2023-1

--- build/assets/js/main.js
+++
@@ -1,1 +0,0 @@
-jQuery(document).ready((function(e){if(e(".reviews-swiper").length){new Swiper(".reviews-swiper",{loop:!0,slidesPerView:1,spaceBetween:0,observer:!0,observeParents:!0,effect:"fade",fadeEffect:{crossFade:!0},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}})}}));
